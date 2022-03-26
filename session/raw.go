30775fe593cf7480044808c35b78cdd4283bf501
lijunxin <623379336@qq.com>
2022-03-26 11:01:09 +0800

    db/sql

---
+++ session/raw.go
@@ -0,0 +1,63 @@
+package session
+
+import (
+	"database/sql"
+	"somnusorm/log"
+	"strings"
+)
+
+type Session struct {
+	db      *sql.DB
+	sql     strings.Builder //当前执行语句
+	sqlVars []interface{}   //当前执行变量
+}
+
+func New(db *sql.DB) *Session {
+	return &Session{
+		db: db,
+	}
+}
+
+func (s *Session) Clear() {
+	s.sql.Reset()   //执行语句清空
+	s.sqlVars = nil ///执行变量清空
+}
+
+func (s *Session) DB() *sql.DB {
+	return s.db
+}
+
+//将执行的语句和变量写入session中
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlVars = append(s.sqlVars, values...)
+	return s
+}
+
+//使用DB，根据sql和sqlVar执行语句
+func (s *Session) Exec() (result sql.Result, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if result, err = s.DB().Exec(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
+
+//查询一行
+func (s *Session) QueryRow() *sql.Row {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	return s.DB().QueryRow(s.sql.String(), s.sqlVars...)
+}
+
+//查询多行
+func (s *Session) QueryRows() (rows *sql.Rows, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if rows, err = s.DB().Query(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
